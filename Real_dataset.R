df4fa796c6bb868e846924b9d20265613f4f4473
AndreinaCastillo <aicasti1@email.arizona.edu>
2016-11-06 11:19:21 -0700

    creating a function that outputs basic values for lifeexp filtering by country name

--- Real_dataset.R
+++ Real_dataset.R
@@ -4,3 +4,33 @@ download.file("https://raw.githubusercontent.com/swcarpentry/r-novice-gapminder/
 
 ?dir.create #Get information about dir.create by typing ? before function
 
+dat <- read.csv("data/gapminder.csv",header=TRUE)
+head(dat)
+summary(dat)
+str(dat) #Str ==> structure NOT string
+
+
+#Function for the analysis of the data
+country_name <- subset(dat,dat$country == "Uganda") #First create a subset of the column we want
+head(country_name)
+summary(country_name)
+
+#Calculate the min, max and mean
+min(dat$lifeExp)
+max(dat$lifeExp)
+mean(dat$lifeExp)
+
+#or
+min(country_name[,5])
+max(country_name[,5])
+mean(country_name[,5])
+
+#Now we make the function
+analyze <- function(country){
+  country_name <- subset(dat, dat$country == country)
+  print(min(country_name[,5]))
+  print(max(country_name[,5]))
+  print(mean(country_name[,5]))
+}
+
+analyze("Uganda")
